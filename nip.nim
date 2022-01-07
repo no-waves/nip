afd981877742dfa377cc31e0b42d9542a6dbd685
vb <51056367+vitalbeach@users.noreply.github.com>
2022-01-06 23:20:36 -0500

    made source a const

--- nip.nim
+++ nip.nim
@@ -1,10 +1,14 @@
 import std/httpclient
 import strutils
 
-# Quick nim package to pull public IP address
+# Quick multi-platform nim package to pull public IP address
+# Can be imported or compiled as stand alone program
+
+const source = "https://icanhazip.com" # Use whatever
+
 proc nip*(): string =
     var client = newHttpClient()
-    var response = getContent(client, "https://icanhazip.com")
+    var response = getContent(client, source)
     return response.strip()
 
 echo nip()
