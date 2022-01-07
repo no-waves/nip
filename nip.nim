4edcbf3b317464e2613d97181bd90e9bd4550d73
Danil Yarantsev (Yardanico) <tiberiumk12@gmail.com>
2022-01-07 13:19:32 +0300

    Fix multiple issues

--- nip.nim
+++ nip.nim
@@ -1,14 +1,19 @@
-import std/httpclient
-import strutils
+## Quick multi-platform nim package to pull public IP address
+## 
+## Can be imported or compiled as stand alone program
 
-# Quick multi-platform nim package to pull public IP address
-# Can be imported or compiled as stand alone program
+import std/[httpclient, strutils]
 
 const source = "https://icanhazip.com" # Use whatever
 
 proc nip*(): string =
+    ## Get the public IP address of the current host 
     var client = newHttpClient()
-    var response = getContent(client, source)
-    return response.strip()
+    try:
+        let response = client.getContent(source)
+        result = response.strip()
+    finally:
+        client.close()
 
-echo nip()
+when isMainModule:
+    echo nip()
--- nip.nimble
+++ nip.nimble
@@ -1,6 +1,6 @@
 # Package
 
-version     = "0.0.1"
+version     = "0.0.2"
 author      = "no-waves"
 description = "simple nim lib to get IP address"
 license     = "MIT"
